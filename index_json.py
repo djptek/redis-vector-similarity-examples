434bfab2f3aed3d23abaa1a768f81dc76da54996
Dominic Page <dominic.page@redis.com>
2023-05-30 20:40:01 +0200

    json query needs bytes to be passed as string

--- index_json.py
+++ index_json.py
@@ -6,6 +6,7 @@ import sys
 import numpy as np
 #from redis.commands.search.field import TextField
 #from redis.commands.search.field import NumericField
+from redis.commands.search.query import Query
 from redis.commands.search.field import VectorField
 from redis.commands.search.indexDefinition import IndexDefinition, IndexType
 
@@ -21,12 +22,24 @@ def int_to_hex(i, bytes, digits):
 del_keys_lua = """local i = 0 for _,v in ipairs(redis.call('KEYS', ARGV[1])) do i 
 = i + redis.call('DEL', v) end return i"""
 
-index_name = 'idx:json:vectors' 
+vectors = []
 vector_field = 'vec'
+
+with open('data.csv', 'r') as infile:
+    reader = csv.reader(infile)
+    for row in reader:
+        vectors.append({vector_field:list(map(int,row))})
+
+print(vectors)
+dimensions = len(vectors[0][vector_field])
+print("DIM {}".format(dimensions))
+
+index_name = 'idx:json:vectors' 
 json_prefix = '$.'
 key_prefix = 'vector:'
 index_defn = IndexDefinition(
-    index_type=IndexType.JSON, prefix=[key_prefix], )
+    index_type=IndexType.JSON, 
+    prefix=[key_prefix])
 schema = (
     VectorField(
         name = '{}{}'.format(
@@ -34,8 +47,8 @@ schema = (
             vector_field),
         algorithm = 'FLAT', 
         attributes = {
-            'TYPE': 'FLOAT32', 
-            'DIM': 3, 
+            'TYPE': 'FLOAT64',
+            'DIM': dimensions, 
             'DISTANCE_METRIC': 'COSINE'},
         as_name = vector_field)
 )
@@ -64,24 +77,14 @@ r.ft(index_name).create_index(
 
 print(r.ft(index_name).info())
 
-vectors = []
-
-with open('data.csv', 'r') as infile:
-    reader = csv.reader(infile)
-    for row in reader:
-        vectors.append({vector_field:list(map(int,row))})
-
 for i, vector in enumerate(vectors):
     # JSON.SET vector:0 $ '{"vec": [176, 7, 28]}'
-    vector_as_bytes = np.array(
-                list(vector[vector_field]), dtype=np.float32).tobytes()
     print(
-        'JSON.SET {}{} $ \'{{"{}": {}}}\'\t# {}'.format(
+        'JSON.SET {}{} $ \'{{"{}": {}}}'.format(
             key_prefix, 
             i, 
             vector_field, 
-            vector[vector_field],
-            vector_as_bytes))
+            vector[vector_field]))
     r.json().set(
         '{}{}'.format(
             key_prefix, 
@@ -90,5 +93,23 @@ for i, vector in enumerate(vectors):
         vector)
 
 ## FT.CREATE idx:json:cli ON JSON PREFIX 1 vector: SCHEMA $.vec AS vec VECTOR FLAT 6 DIM 3 DISTANCE_METRIC L2 TYPE FLOAT32
-## FT.SEARCH idx:json:cli '*=>[KNN 5 @vec $blob AS dist]' SORTBY dist PARAMS 2 blob \x00\x00\x00 DIALECT 2
-## FT.SEARCH idx:json:cli '*=>[KNN 5 @vec $blob AS dist]' SORTBY dist PARAMS 2 blob \x27\x02\x1b DIALECT 2python3
+## FT.SEARCH idx:json:cli '*=>[KNN 5 @vec $blob AS score]' SORTBY score PARAMS 2 blob \x00\x00\x00 DIALECT 2
+## FT.SEARCH idx:json:cli '*=>[KNN 5 @vec $blob AS score]' SORTBY score PARAMS 2 blob \x27\x02\x1b DIALECT 2
+## > FT.SEARCH idx:json:vectors '*=>[KNN 5 @vec $blob AS score]' SORTBY score PARAMS 2 blob \x000027\x000002\x00001b DIALECT 2
+
+query = (
+    Query("*=>[KNN {} @{} $blob AS score]".format(len(vectors)+1, vector_field))
+     .sort_by("score")
+     .return_fields("id", "score")
+     .dialect(2)
+)
+
+for i, vector in enumerate(vectors):
+    query_params = {
+        "blob": np.array(
+                list(vector[vector_field]), dtype=np.int64).tobytes()
+    }    
+    print('Matching {}{} => {}'.format(key_prefix, i, np.array(
+                list(vector[vector_field]), dtype=np.int64).tobytes()))
+    print(r.ft(index_name).search(query, query_params).docs)
+
