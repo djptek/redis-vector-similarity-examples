f9264e2358e2a1fbd7a8260d555c56c1205db7c5
Dominic Page <dominic.page@redis.com>
2023-06-28 09:16:40 +0200

    hybrid pending refactor

--- vector_similarity_hybrid_demo.py
+++ vector_similarity_hybrid_demo.py
@@ -12,12 +12,13 @@ import redis
 import numpy as np
 
 from redis.commands.search.query import Query
-from redis.commands.search.field import VectorField
+from redis.commands.search.field import TagField, VectorField
 from redis.commands.search.indexDefinition import IndexDefinition, IndexType
 
 
 def read_rows():
     """Read csv lines from file into an array of JSON objects containing rows"""
+    # hard code to bikes.csv for first attempt
     rows = []
     with open(file=IN_FILE, mode="r", encoding="UTF-8") as in_file:
         reader = csv.reader(in_file)
@@ -25,67 +26,81 @@ def read_rows():
         for row in reader:
             rows.append(
                 {
+                    headers[0]: row[0],
                     headers[1]: row[1],
-                    headers[4]: row[4],
                     VECTOR_FIELD: np.array(
-                        list([row[0], row[2], row[3]]), dtype=np.float64
+                        list([row[2], row[3], row[4]]), dtype=np.float64
                     ),
                 }
             )
     return rows, len(rows[0]), headers
 
 
-def del_keys_by_prefix(redis_instance):
-    """EVAL Lua to delete a set of keys by prefix * USE AT OWN RISK *"""
-    print(
-        'Deleted {} keys with prefix "{}"'.format(
-            redis_instance.eval(
-                """local i = 0 for _,v in ipairs(redis.call('KEYS', ARGV[1])) 
-do i = i + redis.call('DEL', v) end return i""",
-                0,
-                f"{KEY_PREFIX}*",
-            ),
-            KEY_PREFIX,
+def del_keys_by_uid():
+    """Remove keys matching UIDs of keys to be added"""
+    for row in CSV_ROWS:
+        print(
+            f"""> DEL {
+                row[CSV_HEADERS[0]]}\n{
+                    MY_REDIS.delete(row[CSV_HEADERS[0]])}"""
         )
-    )
 
 
-def drop_index(redis_instance):
+def drop_index():
     """Drop index by name advise if it wasn't there"""
     try:
-        redis_instance.ft(INDEX_NAME).dropindex()
+        MY_REDIS.ft(INDEX_NAME).dropindex()
     except redis.exceptions.ResponseError:
         print("First run vs this Endpoint")
 
 
-def add_rows(redis_instance, rows):
+def add_rows():
     """Traverse array of rows and set these as JSON in Redis"""
-    pipeline = redis_instance.pipeline()
-    for i, row in enumerate(rows):
-        key = f"{KEY_PREFIX}{i}"
+    pipeline = MY_REDIS.pipeline()
+    for i, row in enumerate(CSV_ROWS):
+        print(row)
+        # hard code key to uid
+        key = f"{KEY_PREFIX}:{row[CSV_HEADERS[0]]}"
         if INDEX_TYPE == IndexType.JSON:
-            pipeline.json().set(key, "$", {VECTOR_FIELD: row[VECTOR_FIELD].tolist()})
+            pipeline.json().set(
+                key,
+                "$",
+                {
+                    CSV_HEADERS[1]: row[CSV_HEADERS[1]],
+                    VECTOR_FIELD: row[VECTOR_FIELD].tolist(),
+                },
+            )
         else:
-            pipeline.hset(name=key, mapping={VECTOR_FIELD: row[VECTOR_FIELD].tobytes()})
+            pipeline.hset(
+                name=key,
+                mapping={
+                    CSV_HEADERS[0]: row[0],
+                    CSV_HEADERS[1]: row[1],
+                    VECTOR_FIELD: row[VECTOR_FIELD].tobytes(),
+                },
+            )
         if i % MAX_PIPELINE == 0:
-            print(f"{'HSET' if INDEX_TYPE == IndexType.HASH else 'JSON.SET'} {key} ...")
+            # print(f"{'HSET' if INDEX_TYPE == IndexType.HASH else 'JSON.SET'} {key} ...")
             pipeline.execute()
     pipeline.execute()
 
 
-def search_rows(redis_instance, rows):
+def search_rows():
     """Traverse array of rows and search Redis for these by vector similarity vs index idx"""
-    vs_query = f"*=>[KNN {min(len(rows) + 1, MAX_HITS)} @{VECTOR_FIELD} $blob AS score]"
-    step = max(1, len(rows) // MAX_QUERIES)
-    for i in range(0, step * MAX_QUERIES, step):
-        print(f"\nSearching {INDEX_NAME} by Vector Similarity to row {i} {rows[i]}")
-        blob = rows[i][VECTOR_FIELD].tobytes()
+    vs_query = f"[KNN {min(len(CSV_ROWS), MAX_HITS)} @{VECTOR_FIELD} $blob AS score]"
+    step = max(1, len(CSV_ROWS) // MAX_QUERIES)
+    for i in range(0, min(step * MAX_QUERIES, len(CSV_ROWS)), step):
+        row = CSV_ROWS[i]
+        print(f"\nSearching {INDEX_NAME} by Vector Similarity to row {i} {row}")
+        blob = row[VECTOR_FIELD].tobytes()
+        tag = row[CSV_HEADERS[1]]
+        pf_query = f"(@{CSV_HEADERS[1]}:{{{tag}}})"
         print(
-            redis_instance.ft(INDEX_NAME)
+            MY_REDIS.ft(INDEX_NAME)
             .search(
-                query=Query(vs_query)
+                query=Query(f"{pf_query}=>{vs_query}")
                 .sort_by(field="score", asc=True)
-                .return_fields("id", "score", VECTOR_FIELD)
+                .return_fields("id", "score", CSV_HEADERS[1], VECTOR_FIELD)
                 .dialect(2),
                 query_params={"blob": blob},
             )
@@ -125,48 +140,75 @@ def get_args():
         default=IndexType.HASH,
         help="use index type JSON instead of default HASH",
     )
+    parser.add_argument(
+        "-c",
+        dest="distance_metric",
+        action="store_const",
+        const="COSINE",
+        default="L2",
+        help="use DISTANCE_METRIC COSINE instead of default L2",
+    )
+
     args = parser.parse_args()
-    return args.index_type, args.in_file, args.max_hits, args.max_queries
+    return (
+        args.index_type,
+        args.in_file,
+        args.max_hits,
+        args.max_queries,
+        args.distance_metric,
+    )
 
 
 ###
-INDEX_TYPE, IN_FILE, MAX_HITS, MAX_QUERIES = get_args()
+INDEX_TYPE, IN_FILE, MAX_HITS, MAX_QUERIES, DISTANCE_METRIC = get_args()
 VECTOR_FIELD = "vector"
-INDEX_NAME = f"idx:{INDEX_TYPE.name.lower()}:vectors"
-KEY_PREFIX = f"{VECTOR_FIELD}:"
+KEY_PREFIX = "bikes"
+INDEX_NAME = f"idx:hybrid:{KEY_PREFIX}"
 MAX_PIPELINE = 10000
 
 
 # collect input csv and calculate vector DIM
-csv_rows, dim, csv_headers = read_rows()
+CSV_ROWS, DIM, CSV_HEADERS = read_rows()
 
 # connect to Redis
-my_redis = redis.Redis(
+MY_REDIS = redis.Redis(
     host=os.getenv("RS_HOST"), port=os.getenv("RS_PORT"), password=os.getenv("RS_AUTH")
 )
 
 # tidy up from previous run(s)
-del_keys_by_prefix(redis_instance=my_redis)
-drop_index(redis_instance=my_redis)
+# del_keys_by_prefix()
+del_keys_by_uid()
+drop_index()
 
 # create Vector Similarity index
-my_redis.ft(INDEX_NAME).create_index(
-    fields=(
-        VectorField(
-            name=VECTOR_FIELD if INDEX_TYPE == IndexType.HASH else f"$.{VECTOR_FIELD}",
-            algorithm="FLAT",
-            attributes={"TYPE": "FLOAT64", "DIM": dim, "DISTANCE_METRIC": "L2"},
-            as_name=VECTOR_FIELD,
-        )
+## TagField(
+##        name=CSV_HEADERS[0] if INDEX_TYPE == IndexType.HASH else f"$.{CSV_HEADERS[0]}",
+##        as_name=CSV_HEADERS[0],
+##    ),
+
+SCHEMA = (
+    TagField(
+        name=CSV_HEADERS[1] if INDEX_TYPE == IndexType.HASH else f"$.{CSV_HEADERS[1]}",
+        as_name=CSV_HEADERS[1],
     ),
-    definition=IndexDefinition(index_type=INDEX_TYPE, prefix=[KEY_PREFIX]),
+    VectorField(
+        name=VECTOR_FIELD if INDEX_TYPE == IndexType.HASH else f"$.{VECTOR_FIELD}",
+        algorithm="FLAT",
+        attributes={
+            "TYPE": "FLOAT64",
+            "DIM": DIM,
+            "DISTANCE_METRIC": DISTANCE_METRIC,
+        },
+        as_name=VECTOR_FIELD,
+    ),
+)
+MY_REDIS.ft(INDEX_NAME).create_index(
+    fields=SCHEMA,
+    definition=IndexDefinition(index_type=INDEX_TYPE, prefix=[f"{KEY_PREFIX}:"]),
 )
 
 # index the data as rows
-add_rows(
-    redis_instance=my_redis,
-    rows=csv_rows,
-)
+add_rows()
 
 # run vector similarity searches over the data
-search_rows(redis_instance=my_redis, rows=csv_rows)
+search_rows()
